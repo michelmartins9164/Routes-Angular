92f10220cd57077e2490645f6ee686081f8f0c93
michelmartins9164 <michelmartins9164@gmail.com>
2022-07-27 11:17:21 -0300

    routes

---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,15 @@
+import { HomeModule } from './home.module';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '',pathMatch: 'full', component: HomeComponent }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
